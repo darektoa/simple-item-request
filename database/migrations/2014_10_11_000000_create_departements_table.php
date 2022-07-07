f55713061a2f4742f4894196dc7717b424e5f094
darektoa <darektoa@gmail.com>
2022-07-08 05:02:24 +0700

    Add: Fields in departements table migration

--- database/migrations/2014_10_11_000000_create_departements_table.php
+++ database/migrations/2014_10_11_000000_create_departements_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('departements', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
