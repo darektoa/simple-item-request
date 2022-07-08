9335cf2d72ad5d903e57da2e39c10c20f5341017
darektoa <darektoa@gmail.com>
2022-07-09 05:02:43 +0700

    Add: Fields in user table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('departement_id')->constrained();
+            $table->string('nik', 20);
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
