d6318af591a4b7877d5e88420fbccf270b420bb5
darektoa <darektoa@gmail.com>
2022-07-09 05:05:17 +0700

    Add: Fields in stuff_requests table migration

--- database/migrations/2022_07_18_084826_create_stuff_requests_table.php
+++ database/migrations/2022_07_18_084826_create_stuff_requests_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('stuff_requests', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
             $table->timestamps();
         });
     }
