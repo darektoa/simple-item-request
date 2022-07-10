dcb8a80fd7486c042dc1c533565399b8267c7d9d
darektoa <darektoa@gmail.com>
2022-07-11 05:04:58 +0700

    Add: Stuffs relation method in stuff request model

--- app/Models/StuffRequest.php
+++ app/Models/StuffRequest.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\{BelongsTo, BelongsToMany};
 
 class StuffRequest extends Model
 {
@@ -15,5 +15,10 @@ class StuffRequest extends Model
 
     public function user() :BelongsTo {
         return $this->belongsTo(User::class);
+    }
+
+
+    public function stuffs() :BelongsToMany {
+        return $this->belongsToMany(Stuff::class);
     }
 }
