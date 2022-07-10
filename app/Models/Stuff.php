9a33a4cbcf061e9bc98b4ce2a80bea1cc7a9ff65
darektoa <darektoa@gmail.com>
2022-07-11 05:05:26 +0700

    Add: Requests relation method in stuff model

--- app/Models/Stuff.php
+++ app/Models/Stuff.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Stuff extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+
+    public function requests() :BelongsToMany {
+        return $this->belongsToMany(StuffRequest::class);
+    }
 }
