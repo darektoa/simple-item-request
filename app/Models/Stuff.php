0fe6da441b4a1472b66bc4df2d3056371accfd03
darektoa <darektoa@gmail.com>
2022-07-12 05:03:14 +0700

    Add: Status name attribute method in stuff model

--- app/Models/Stuff.php
+++ app/Models/Stuff.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -21,5 +22,14 @@ class Stuff extends Model
 
     public function requests() :BelongsToMany {
         return $this->belongsToMany(StuffRequest::class);
+    }
+
+
+    public function statusName() :Attribute {
+        $get = fn() => $this->statusNames[$this->status] ?? 'Unknown';
+        
+        return Attribute::make(
+            get: $get
+        );
     }
 }
