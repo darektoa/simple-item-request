69888bef2a8eb08e17c78bb3296fb8bb6b632641
darektoa <darektoa@gmail.com>
2022-07-14 05:05:38 +0700

    Add: Use soft deletes trait in stuff model

--- app/Models/Stuff.php
+++ app/Models/Stuff.php
@@ -6,10 +6,11 @@ use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Stuff extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $appends = ['status_name', 'unit_name'];
 
