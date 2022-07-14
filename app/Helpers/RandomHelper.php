27d2f7ec9072a0f077f7f92ccdf48c9cb0d2ff18
darektoa <darektoa@gmail.com>
2022-07-15 05:02:41 +0700

    Add: Code static method in random helper

--- app/Helpers/RandomHelper.php
+++ app/Helpers/RandomHelper.php
@@ -3,5 +3,10 @@
 namespace App\Helpers;
 
 class RandomHelper{
-    
+    static public function code($length=8) {
+        $code   = random_bytes($length/2);
+        $code   = bin2hex($code);
+        
+        return $code;
+    }
 }
