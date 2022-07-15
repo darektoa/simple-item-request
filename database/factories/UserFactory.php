01c7107c6ad1c9d697ab938ddc3288e9c62daa66
darektoa <darektoa@gmail.com>
2022-07-16 05:04:43 +0700

    Add: Properties in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Departement;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
@@ -17,12 +18,15 @@ class UserFactory extends Factory
      */
     public function definition()
     {
+        $faker = fake('id_ID');
+
         return [
-            'name' => fake()->name(),
-            'email' => fake()->safeEmail(),
+            'departement_id'    => Departement::inRandomOrder()->first()->id,
+            'nik'               => $faker->nik(),
+            'name'              => $faker->name(),
+            'email'             => $faker->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ];
     }
 
