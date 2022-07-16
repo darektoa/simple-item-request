3b5a23983c290bfacc59b1502860b3447ff4e7a7
darektoa <darektoa@gmail.com>
2022-07-17 05:03:42 +0700

    Fix: Typo at departement seeder name

--- database/factories/DepartementFactory.php
+++ database/factories/DepartementFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
  */
-class DepartmentFactory extends Factory
+class DepartementFactory extends Factory
 {
     /**
      * Define the model's default state.
