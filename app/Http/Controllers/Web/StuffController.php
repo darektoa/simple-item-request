c23441c657452f3ec53fe6ea6c1487e818520cca
darektoa <darektoa@gmail.com>
2022-07-18 05:05:15 +0700

    Add: Index method in stuff web controller

--- app/Http/Controllers/Web/StuffController.php
+++ app/Http/Controllers/Web/StuffController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\Stuff;
 use Illuminate\Http\Request;
 
 class StuffController extends Controller
 {
-    //
+    public function index() {
+        $stuffs = Stuff::all();
+
+        return view('pages.stuff.index', compact('stuffs'));
+    }
 }
