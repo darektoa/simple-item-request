07b1c9e58916abb082c15dc6ffb025e710a23ebc
darektoa <darektoa@gmail.com>
2022-07-19 05:02:12 +0700

    Add: Stuff list route in web routes

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,7 @@
 
 namespace App\Http\Controllers\Web;
 use Illuminate\Support\Facades\Route;
+
+Route::prefix('/stuffs')->group(function() {
+    Route::get('/', [StuffController::class, 'index']);
+});
