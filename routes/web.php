ed030dd8f23bbba330c38091c3987aaa27bd1d55
darektoa <darektoa@gmail.com>
2022-07-19 05:03:44 +0700

    Add: Redirect root route to stuff list route in web routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Web;
 use Illuminate\Support\Facades\Route;
 
+Route::redirect('/', '/stuffs');
+
 Route::prefix('/stuffs')->group(function() {
     Route::get('/', [StuffController::class, 'index']);
 });
