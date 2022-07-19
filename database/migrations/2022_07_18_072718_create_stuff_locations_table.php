44e3f833f5e030fcd735bc0e2ef865def2461fc5
darektoa <darektoa@gmail.com>
2022-07-20 05:04:04 +0700

    Add: Fields in stuff_locations table migration

--- database/migrations/2022_07_18_072718_create_stuff_locations_table.php
+++ database/migrations/2022_07_18_072718_create_stuff_locations_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('stuff_locations', function (Blueprint $table) {
             $table->id();
+            $table->string('code', 10);
+            $table->string('name', 100);
             $table->timestamps();
         });
     }
