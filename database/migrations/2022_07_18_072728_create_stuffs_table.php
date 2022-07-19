e3eafbb3f52fe18ca41456deb4867cf73bc5efb2
darektoa <darektoa@gmail.com>
2022-07-20 05:06:11 +0700

    Add: Unique constraint at code field in stuffs table migration

--- database/migrations/2022_07_18_072728_create_stuffs_table.php
+++ database/migrations/2022_07_18_072728_create_stuffs_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('stuffs', function (Blueprint $table) {
             $table->id();
             $table->foreignId('stuff_location_id')->constrained();
-            $table->string('code', 10);
+            $table->string('code', 10)->unique();
             $table->string('name', 100);
             $table->bigInteger('stock')->default(0);
             $table->smallInteger('unit')->default(1);
