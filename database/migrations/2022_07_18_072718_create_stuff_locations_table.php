12ae2072149ae30aaee973c68d22112883f538a4
darektoa <darektoa@gmail.com>
2022-07-21 05:05:19 +0700

    Add: Soft deletes field in stuff locations table migration

--- database/migrations/2022_07_18_072718_create_stuff_locations_table.php
+++ database/migrations/2022_07_18_072718_create_stuff_locations_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('code', 10)->unique();
             $table->string('name', 100);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
