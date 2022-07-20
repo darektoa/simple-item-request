f1c6ac5f153f870c00b57650b8afbdb5e6a363a0
darektoa <darektoa@gmail.com>
2022-07-21 05:06:17 +0700

    Add: Using soft deletes trait in stuff locations model

--- app/Models/StuffLocation.php
+++ app/Models/StuffLocation.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class StuffLocation extends Model
 {
-    use HasFactory;
+    use HasFactory, softDeletes;
 }
