298dbe2ab88ddb4dc6f3e9c841d159527011ee5a
darektoa <darektoa@gmail.com>
2022-07-22 05:02:44 +0700

    Add: Stuffs relation method in stuff location model

--- app/Models/StuffLocation.php
+++ app/Models/StuffLocation.php
@@ -5,8 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class StuffLocation extends Model
 {
     use HasFactory, softDeletes;
+
+    
+    public function stuffs() :HasMany {
+        return $this->hasMany(Stuff::class);
+    }
 }
