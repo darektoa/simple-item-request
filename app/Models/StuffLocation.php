7329f64368cfe9bd5b8e0438a7cad12293b041c0
darektoa <darektoa@gmail.com>
2022-07-22 05:05:54 +0700

    Add: Code default value in boot overiding method of stuff location model

--- app/Models/StuffLocation.php
+++ app/Models/StuffLocation.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Helpers\RandomHelper;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\softDeletes;
@@ -10,6 +11,16 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 class StuffLocation extends Model
 {
     use HasFactory, softDeletes;
+
+
+    protected static function boot() {
+        parent::creating(function($data) {
+            if($data->code) return;
+            $data->code = 'SL-' . RandomHelper::code(3);
+        });
+
+        parent::boot();
+    }
 
     
     public function stuffs() :HasMany {
