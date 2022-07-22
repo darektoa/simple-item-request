f26330af45507a85f619f41f9571dea3381532c3
darektoa <darektoa@gmail.com>
2022-07-23 05:03:17 +0700

    Add: Get with location relation in index method of stuff web controller

--- app/Http/Controllers/Web/StuffController.php
+++ app/Http/Controllers/Web/StuffController.php
@@ -9,7 +9,7 @@ use Illuminate\Http\Request;
 class StuffController extends Controller
 {
     public function index() {
-        $stuffs = Stuff::paginate(5);
+        $stuffs = Stuff::with(['location'])->paginate(5);
 
         return view('pages.stuff.index', compact('stuffs'));
     }
