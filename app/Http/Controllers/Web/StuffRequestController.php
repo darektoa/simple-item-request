aa2b3776243f0a40a5d3450fd6af0bfbccab87ac
darektoa <darektoa@gmail.com>
2022-07-23 05:05:21 +0700

    Add: Index method in stuff request web controller

--- app/Http/Controllers/Web/StuffRequestController.php
+++ app/Http/Controllers/Web/StuffRequestController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
+use App\Models\StuffRequest;
 use Illuminate\Http\Request;
 
 class StuffRequestController extends Controller
 {
-    //
+    public function index() {
+        $stuffRequests = StuffRequest::with(['user', 'stuffs'])
+            ->paginate(10);
+
+        return view('pages.stuff-request.index', compact('stuffRequests'));
+    }
 }
