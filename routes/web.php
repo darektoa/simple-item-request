eb46e0b480723772076d527565e99a9c7691f292
darektoa <darektoa@gmail.com>
2022-07-24 05:02:32 +0700

    Add: Stuff request create route in web routes

--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Route::redirect('/', '/stuffs');
 Route::prefix('/stuffs')->name('stuffs.')->group(function() {
     Route::get('/', [StuffController::class, 'index'])->name('index');
     Route::get('/requests', [StuffRequestController::class, 'index'])->name('requests.index');
+    Route::get('/requests/create', [StuffRequestController::class, 'create'])->name('requests.create');
 });
