ef1a696ab25e37038f0a705912c911c84dff9839
darektoa <darektoa@gmail.com>
2022-07-24 05:04:22 +0700

    Add: Stuff requests for post route in web routes

--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,6 @@ Route::redirect('/', '/stuffs');
 Route::prefix('/stuffs')->name('stuffs.')->group(function() {
     Route::get('/', [StuffController::class, 'index'])->name('index');
     Route::get('/requests', [StuffRequestController::class, 'index'])->name('requests.index');
+    Route::post('/requests', [StuffRequestController::class, 'store'])->name('requests.store');
     Route::get('/requests/create', [StuffRequestController::class, 'create'])->name('requests.create');
 });
