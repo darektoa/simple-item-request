3198ccf2e77f7bf484fac52dd4b91ed860c47921
darektoa <darektoa@gmail.com>
2022-07-25 05:02:17 +0700

    Fix: Store method in stuff request web controller

--- app/Http/Controllers/Web/StuffRequestController.php
+++ app/Http/Controllers/Web/StuffRequestController.php
@@ -11,6 +11,7 @@ class StuffRequestController extends Controller
 {
     public function index() {
         $stuffRequests = StuffRequest::with(['user', 'stuffs'])
+            ->latest()
             ->paginate(10);
 
         return view('pages.stuff-request.index', compact('stuffRequests'));
@@ -35,15 +36,18 @@ class StuffRequestController extends Controller
             ]);
 
             $stuffs = collect($request->stuffs)
-                ->mapWithKeys(fn($item) => [
-                    $item['id'] => ['quantity' => $item['quantity']]
-                ]);
+                ->groupBy('id')
+                ->map(function($item) {
+                    $quantity = $item['quantity'] ?? $item->sum('quantity');
+                    return ['quantity' => $quantity];
+                });
 
             $stuffRequest = StuffRequest::create(['user_id' => 1]); // Example User
             $stuffRequest->stuffs()->attach($stuffs->toArray());
 
             return redirect()->route('stuffs.requests.index');
         }catch(Exception $err){
+            dd($err->getMessage());
             return back()->withInput();
         }
     }
